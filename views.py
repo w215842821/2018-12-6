d00cd42cbe3159decf56e51c92994d871b9f1a63
w215842821 <215842821@qq.com>
2018-12-07 14:52:55 +0800

    创建login

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redrect
 
 def index(request):
     return HttpResponse('index')
 
+def login(request):
+    return redirect('/index')
